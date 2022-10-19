dc0ff49a47c9d046026f0d4d8a31d2971a0f32c0
juancito.eth <juancitoeth@gmail.com>
2022-10-19 05:01:26 -0300

    change order

--- components/TabSelector.tsx
+++ components/TabSelector.tsx
@@ -3,9 +3,9 @@ import styles from "@styles/TabSelector.module.css";
 const TabSelector = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.selected}>Tokens</div>
+      <div className={styles.selected}>History</div>
+      <div className={styles.tab}>Tokens</div>
       <div className={styles.tab}>NFTs</div>
-      <div className={styles.tab}>History</div>
     </div>
   );
 };
