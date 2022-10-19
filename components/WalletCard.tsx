00c208ba193603ddb92ad29fb081de04eeea644c
juancito.eth <juancitoeth@gmail.com>
2022-10-19 01:50:42 -0300

    add button

--- components/WalletCard.tsx
+++ components/WalletCard.tsx
@@ -1,19 +1,40 @@
 import styles from "@styles/WalletCard.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Identicon from "react-identicons";
 
 const WalletCard = () => {
+  const router = useRouter();
+
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    const { address = "" } = router.query;
+    setAddress(address as string);
+  }, [router]);
+
+  const shortenAddress = (address: string) => {
+    const start = address.slice(0, 6);
+    const end = address.slice(address.length - 4, address.length);
+    return `${start}...${end}`;
+  };
+
   return (
     <div className={styles.container}>
-      <Image
-        height="104"
-        width="104"
-        src={"/profile.png"}
-        alt="profile"
-        className={styles.profileImg}
-      ></Image>
-      <div className={styles.dataContainer}>
-        <div className={styles.address}>0x2491…5818</div>
-        <div className={styles.balance}>$323.29</div>
+      <div>
+        <Identicon
+          string={address}
+          className={styles.profileImg}
+          size={"104"}
+        ></Identicon>
+        <div className={styles.dataContainer}>
+          <div className={styles.address}>{shortenAddress(address)}</div>
+          <div className={styles.balance}>$323.29</div>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.addButton}>Add wallet</button>
       </div>
     </div>
   );
